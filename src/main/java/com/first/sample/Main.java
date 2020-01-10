98d71a03104f8e2265697f7061ffa40108b348fc
id851734 <neeraj.gupta1@rsystems.com>
2020-01-10 13:48:47 +0530

    Modified suggetsed changes

--- src/main/java/com/first/sample/Main.java
+++ src/main/java/com/first/sample/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String args[]){
 		
-		System.out.println("Sample First Maven Project");
+		System.out.println("First Sample  Maven Project");
 	}
 	
 }
